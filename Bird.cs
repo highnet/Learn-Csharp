6a202fb218ed656a4c9df95e6aaa9a5b58b991b3
joaquin telleria <joaquintelleria@gmail.com>
2019-09-24 20:41:58 +0200

    Update Bird.cs

--- Bird.cs
+++ Bird.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour {
 
-    public Vector3 moveDirection = Vector3.forward;
+    public Vector3 movementV = Vector3.forward;
+    [Range(0.0F, 10.0F)]
+    public float alignmentRadius = 5f;
 
     private void FixedUpdate()
     {
-        transform.Translate(moveDirection * Time.deltaTime); // move
+        DoAlignment();
+        transform.Translate(movementV * Time.deltaTime); // move
+    }
+
+    private void LateUpdate()
+    {
+        transform.rotation.eulerAngles.Set(transform.rotation.eulerAngles.x,90,-90);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -18,14 +26,40 @@ public class Bird : MonoBehaviour {
         }
     }
 
-    public void ReflectProjectile()
+    private void DoAlignment()
+    {
+        Collider[] hits = Physics.OverlapSphere(this.transform.position, alignmentRadius);
+        int neighborCount = 0;
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.gameObject != this.gameObject && hit.gameObject.tag.Contains("bird"))
+            {
+                {
+                    transform.rotation.eulerAngles.Set(transform.rotation.eulerAngles.x + hit.transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + hit.transform.rotation.eulerAngles.y, -90);
+                    neighborCount++;
+                }
+            }
+        }
+        if (neighborCount == 0)
+        {
+            return;
+        }
+        this.movementV.x /= neighborCount;
+        this.movementV.y /= neighborCount;
+        this.movementV.Normalize();
+
+    }
+
+    private void ReflectProjectile()
     {
         RaycastHit hit;
-        Ray ray = new Ray(transform.position, moveDirection);
+        Ray ray = new Ray(transform.position, transform.forward);
 
         if (Physics.Raycast(ray, out hit))
         {
-            moveDirection = Vector3.Reflect(moveDirection, hit.normal);
+
+            transform.rotation = Quaternion.LookRotation(Vector3.Reflect(transform.forward, hit.normal),Vector3.back);
         }
     }
 }
